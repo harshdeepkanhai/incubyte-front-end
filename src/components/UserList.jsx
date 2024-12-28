151c9f232e83a8e87e660ecc3d9c1041943a2dec
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-12-28 21:29:22 +0530

    Add UserList component

---
+++ src/components/UserList.jsx
@@ -0,0 +1,15 @@
+const UserList = ({ users, onEditUser, onDeleteUser }) => {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.name} ({user.email})
+          <button onClick={() => onEditUser(user)}>Edit</button>
+          <button onClick={() => onDeleteUser(user.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
