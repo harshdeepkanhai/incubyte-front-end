69e57b0e26a797ee5455f85900cda7aa58702236
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-12-28 22:12:30 +0530

    Add UserList components and its stylesheet

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,13 +1,19 @@
 import React from "react";
+import "./UserList.css";
 
 const UserList = ({ users, onEditUser, onDeleteUser }) => {
   return (
     <ul>
       {users.map((user) => (
         <li key={user.id}>
-          {user.name} ({user.email})
-          <button onClick={() => onEditUser(user)}>Edit</button>
-          <button onClick={() => onDeleteUser(user.id)}>Delete</button>
+          <div>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+          <div>
+            <button onClick={() => onEditUser(user)}>Edit</button>
+            <button onClick={() => onDeleteUser(user.id)}>Delete</button>
+          </div>
         </li>
       ))}
     </ul>
